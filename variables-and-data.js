f2f510ee2ffb74e24db2bb2b44aa0df9218e139a
Adriana Martinez <amartinezguzma@cnm.edu>
2021-06-23 15:54:18 -0600

    Created sample coding for variables, data types, and typing

---
+++ variables-and-data.js
@@ -0,0 +1,52 @@
+/*
+*Variables
+ */
+let wordData;
+wordData= "Word list";
+
+console.log (wordData)
+
+let x = 100;
+let y = x *0.07;
+let z = x + y;
+
+let subTotal = 100;
+let tax= subTotal * 0.07
+let totalWithTax = tax + subTotal
+
+/*
+*Data Types
+ */
+
+let obj = {
+    key1: "value 1",
+    key2: 4,
+    boolean: true,
+    obj2:{
+        obj2key1: "value of inner object",
+    }
+};
+
+console.log (obj.obj2.obj2key1);
+
+/*
+*Typing
+ */
+
+let variable = "string"
+
+let var1= "2";
+let var2= 5;
+let results = var1 + var2
+
+console.log (results)
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 5;
+let division= 4 / 2
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2"
+
+console.log (string)
